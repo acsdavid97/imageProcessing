ae7ecb8e12460bec5d2496d4285a5752fa2c1e7b
Acs David <acs_david97@yahoo.com>
2016-07-15 13:04:00 +0300

    test pictures in a folder

--- reading&displaying.py
+++ reading&displaying.py
@@ -3,8 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv2.imread('messi5.jpg', 0)
-img2 = img.copy()
-template = cv2.imread('template.jpg', 0)
+print img.shape
 
 cv2.imshow('image', img)
 cv2.waitKey(0)
