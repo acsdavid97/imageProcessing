1c33ed3eb56fce2d4f6a762c2ff6815242626b4d
Acs David <acs_david97@yahoo.com>
2016-07-23 14:19:07 +0300

    minor changes

--- features/featureMatching.py
+++ features/featureMatching.py
@@ -2,20 +2,21 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img1 = cv2.imread('book.jpg', 0)
-img2 = cv2.imread('bookInContext.jpg', 0)
+img1 = cv2.imread('book.jpg')
+img2 = cv2.imread('bookInContext.jpg')
 
-orb = cv2.ORB_create(100)
+orb = cv2.ORB_create(500, 1.1, 16, 40, patchSize=40)
 
 kp1, des1 = orb.detectAndCompute(img1, None)
 kp2, des2 = orb.detectAndCompute(img2, None)
 
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, True)
-
+img3 = np.hstack((img1, img2))
 matches = bf.match(des1, des2)
 
-matches = sorted(matches, key = lambda x:x.distance)
+matches = sorted(matches, key=lambda x: x.distance)
+cv2.drawMatches(img1, kp1, img2, kp2, matches[:50], img3, (0, 255, 0), flags=2)
 
-img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:10], flags=2)
-
-plt.imshow(img3), plt.show()
+cv2.imshow('img3', img3)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
